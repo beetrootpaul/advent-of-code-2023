e810d5b50572aabeec47e844871746b8f0bac94e
Beetroot Paul <paul@beetrootpaul.com>
2023-12-20 00:54:16 +0100

    day19: parse

--- unity/Assets/Scripts/Day19Part1Code.cs
+++ unity/Assets/Scripts/Day19Part1Code.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections.Generic;
 using System.IO;
 using Cysharp.Threading.Tasks;
 using TMPro;
@@ -5,6 +7,38 @@ using UnityEngine;
 
 namespace aoc2023
 {
+    internal struct Rating
+    {
+        public readonly int X;
+        public readonly int M;
+        public readonly int A;
+        public readonly int S;
+
+        public Rating(int x, int m, int a, int s)
+        {
+            X = x;
+            M = m;
+            A = a;
+            S = s;
+        }
+    }
+
+    internal struct Step
+    {
+        public readonly Predicate<Rating> Condition;
+        public readonly bool IsAccepted;
+        public readonly bool IsRejected;
+        public readonly string NextWorkflowId;
+
+        public Step(Predicate<Rating> condition, bool isAccepted, bool isRejected, string nextWorkflowId)
+        {
+            Condition = condition;
+            IsAccepted = isAccepted;
+            IsRejected = isRejected;
+            NextWorkflowId = nextWorkflowId;
+        }
+    }
+
     //
     // https://adventofcode.com/2023/day/19
     //
@@ -26,16 +60,34 @@ namespace aoc2023
         {
             resultText.text = "...";
 
-            await ComputeFor(inputFile switch
+            var (workflows, ratings) = await Parse(inputFile switch
             {
                 Input.Example1 => "day19/example1_in.txt",
                 Input.Puzzle1 => "day19/puzzle1_in.txt",
                 _ => "NOT_SET"
             });
+            foreach (var workflow in workflows)
+            {
+                print($":: {workflow.Key} ::");
+                foreach (var step in workflow.Value)
+                {
+                    print($"A:{step.IsAccepted} | R:{step.IsRejected} | nW:{step.NextWorkflowId}");
+                }
+            }
+            foreach (var rating in ratings)
+            {
+                print($"RATING: x={rating.X},m={rating.M},a={rating.A},s={rating.S}");
+            }
         }
 
-        private async UniTask ComputeFor(string file)
+        private async UniTask<(
+            Dictionary<string, List<Step>>,
+            List<Rating>
+            )> Parse(string file)
         {
+            Dictionary<string, List<Step>> workflows = new();
+            List<Rating> ratings = new();
+
             string[] lines;
             try
             {
@@ -43,10 +95,131 @@ namespace aoc2023
             }
             catch
             {
-                return;
+                return (workflows, ratings);
             }
 
-            print(string.Join("\n", lines));
+            var isReadingWorkflows = true;
+            foreach (var line in lines)
+            {
+                if (line.Trim().Length == 0)
+                {
+                    isReadingWorkflows = false;
+                    continue;
+                }
+
+                if (isReadingWorkflows)
+                {
+                    var line2 = line.Split("}")[0];
+                    var workflowId = line2.Split("{")[0];
+                    var rules = line2.Split("{")[1];
+
+                    var workflowSteps = new List<Step>();
+                    workflows.Add(workflowId, workflowSteps);
+
+                    foreach (var rule in rules.Split(","))
+                    {
+                        var isAccepted = false;
+                        var isRejected = false;
+                        var nextWorkflowId = "";
+                        Predicate<Rating> condition = _ => true;
+
+                        var tokens = rule.Split(":");
+                        if (tokens.Length == 1)
+                        {
+                            switch (tokens[0])
+                            {
+                                case "A":
+                                    isAccepted = true;
+                                    break;
+                                case "R":
+                                    isRejected = true;
+                                    break;
+                                default:
+                                    nextWorkflowId = tokens[0];
+                                    break;
+                            }
+                        }
+                        else
+                        {
+                            switch (tokens[1])
+                            {
+                                case "A":
+                                    isAccepted = true;
+                                    break;
+                                case "R":
+                                    isRejected = true;
+                                    break;
+                                default:
+                                    nextWorkflowId = tokens[1];
+                                    break;
+                            }
+                            if (tokens[0].Split("=").Length > 1)
+                            {
+                                var category = tokens[0].Split("=")[0];
+                                var number = int.Parse(tokens[0].Split("=")[1]);
+                                condition = rating =>
+                                {
+                                    return category switch
+                                    {
+                                        "x" => rating.X == number,
+                                        "m" => rating.M == number,
+                                        "a" => rating.A == number,
+                                        "s" => rating.S == number,
+                                        _ => false
+                                    };
+                                };
+                            }
+                            else if (tokens[0].Split("<").Length > 1)
+                            {
+                                var category = tokens[0].Split("<")[0];
+                                var number = int.Parse(tokens[0].Split("<")[1]);
+                                condition = rating =>
+                                {
+                                    return category switch
+                                    {
+                                        "x" => rating.X < number,
+                                        "m" => rating.M < number,
+                                        "a" => rating.A < number,
+                                        "s" => rating.S < number,
+                                        _ => false
+                                    };
+                                };
+                            }
+                            else if (tokens[0].Split(">").Length > 1)
+                            {
+                                var category = tokens[0].Split(">")[0];
+                                var number = int.Parse(tokens[0].Split(">")[1]);
+                                condition = rating =>
+                                {
+                                    return category switch
+                                    {
+                                        "x" => rating.X > number,
+                                        "m" => rating.M > number,
+                                        "a" => rating.A > number,
+                                        "s" => rating.S > number,
+                                        _ => false
+                                    };
+                                };
+                            }
+                        }
+
+                        workflowSteps.Add(new Step(condition, isAccepted, isRejected, nextWorkflowId));
+                    }
+                }
+                else
+                {
+                    var line2 = line.Split("{")[1].Split("}")[0];
+                    var categoryRatings = line2.Split(",");
+                    ratings.Add(new Rating(
+                        int.Parse(categoryRatings[0].Split("=")[1]),
+                        int.Parse(categoryRatings[1].Split("=")[1]),
+                        int.Parse(categoryRatings[2].Split("=")[1]),
+                        int.Parse(categoryRatings[3].Split("=")[1])
+                    ));
+                }
+            }
+
+            return (workflows, ratings);
         }
     }
 }
