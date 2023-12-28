23b333b11c04aba6f26946f2878ce9b1f300973c
Beetroot Paul <paul@beetrootpaul.com>
2023-12-28 10:57:55 +0100

    Godot: make script formatted badly, for testing purposes

--- godot/day23/day_23_part_1.gd
+++ godot/day23/day_23_part_1.gd
@@ -1,14 +1,17 @@
 extends Node2D
-
-
-func _ready():
-	var filePath = "res://day23/example1_in.txt"
+func _ready()   :
+	var filePath       = "res://day23/example1_in.txt"
 	#var filePath = "res://day23/puzzle1_in.txt"
 
-	var input = FileAccess.open(filePath, FileAccess.READ).get_as_text()
+	var input =        FileAccess.open(filePath, FileAccess.READ).get_as_text()
 	print_debug(input)
 	var lines = input.split("\n")
 
 
-func _process(delta):
+
+
+
+
+
+func       _process(delta):
 	pass
