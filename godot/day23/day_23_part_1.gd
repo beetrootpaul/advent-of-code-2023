e934b656ab9521b39336852f94cc4fa2fec4ea4a
Beetroot Paul <paul@beetrootpaul.com>
2023-12-31 13:55:07 +0100

    day23: access a tilemap in the scene's script

--- godot/day23/day_23_part_1.gd
+++ godot/day23/day_23_part_1.gd
@@ -1,5 +1,7 @@
 extends Node2D
 
+@export var tilemap: TileMap
+
 
 func _ready():
 	var filePath = "res://day23/example1_in.txt"
@@ -8,6 +10,9 @@ func _ready():
 	var input = FileAccess.open(filePath, FileAccess.READ).get_as_text()
 	print_debug(input)
 	var lines = input.split("\n")
+
+	# TODO: tmp
+	remove_child(tilemap)
 
 
 func _process(delta):
