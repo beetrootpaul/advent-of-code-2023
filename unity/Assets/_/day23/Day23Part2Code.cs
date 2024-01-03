f332e23fae175e2c9ba2139f16f9f89649f2068b
Beetroot Paul <paul@beetrootpaul.com>
2024-01-03 01:44:08 +0100

    day23: draw the forest

--- unity/Assets/_/day23/Day23Part2Code.cs
+++ unity/Assets/_/day23/Day23Part2Code.cs
@@ -1,6 +1,8 @@
 using System.IO;
+using System.Linq;
 using TMPro;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 namespace aoc2023.Day23
 {
@@ -16,7 +18,26 @@ namespace aoc2023.Day23
         }
 
         [field: SerializeField] private TextMeshProUGUI ResultText { get; set; }
+        [field: SerializeField] private Tilemap ForestTilemap { get; set; }
+        [field: SerializeField] private Tilemap PathTilemap { get; set; }
+        [field: SerializeField] private TileBase PathTile { get; set; }
+        [field: SerializeField] private TileBase ForestFullTile { get; set; }
+        [field: SerializeField] private TileBase ForestTBTile { get; set; }
+        [field: SerializeField] private TileBase ForestLRTile { get; set; }
+        [field: SerializeField] private TileBase ForestTLTile { get; set; }
+        [field: SerializeField] private TileBase ForestTRTile { get; set; }
+        [field: SerializeField] private TileBase ForestBLTile { get; set; }
+        [field: SerializeField] private TileBase ForestBRTile { get; set; }
+        [field: SerializeField] private TileBase ForestTTile { get; set; }
+        [field: SerializeField] private TileBase ForestBTile { get; set; }
+        [field: SerializeField] private TileBase ForestLTile { get; set; }
+        [field: SerializeField] private TileBase ForestRTile { get; set; }
+
         [field: SerializeField] private Input InputFile { get; set; }
+
+        private char[][] _tiles = { };
+        private int _rows = 1;
+        private int _cols = 1;
 
         private void Start()
         {
@@ -29,13 +50,66 @@ namespace aoc2023.Day23
                 _ => "NOT_SET"
             });
 
+            DrawTiles();
+
             ResultText.text = "DONE";
         }
 
-        private static void Parse(string inputFilePath)
+        private void Parse(string inputFilePath)
         {
             var lines = File.ReadAllLines($"{Application.streamingAssetsPath}/{inputFilePath}");
-            print(string.Join('\n', lines));
+            _tiles = lines
+                .Select(line => line.Trim())
+                .Where(line => line.Length > 0)
+                .Select(line => line.ToCharArray())
+                .ToArray();
+            _rows = _tiles.Length;
+            _cols = _tiles[0].Length;
+            print(string.Join('\n', _tiles.Select(tilesRow => string.Join("", tilesRow))));
+        }
+
+        private void DrawTiles()
+        {
+            PathTilemap.ClearAllTiles();
+            ForestTilemap.ClearAllTiles();
+
+            for (var row = 0; row < _rows; row++)
+            {
+                for (var col = 0; col < _cols; col++)
+                {
+                    var tilemapXy = new Vector3Int(col, _rows - row - 1);
+
+                    PathTilemap.SetTile(tilemapXy, PathTile);
+
+                    if (_tiles[row][col] == '#')
+                    {
+                        ForestTilemap.SetTile(tilemapXy, ForestFullTile);
+                    }
+                    else
+                    {
+                        var t = row <= 0 || _tiles[row - 1][col] == '#';
+                        var b = row >= _rows - 1 || _tiles[row + 1][col] == '#';
+                        var l = col <= 0 || _tiles[row][col - 1] == '#';
+                        var r = col >= _cols - 1 || _tiles[row][col + 1] == '#';
+                        var mask = (t ? 0b1000 : 0) + (b ? 0b0100 : 0) + (l ? 0b0010 : 0) + (r ? 0b0001 : 0);
+                        var forestTile = mask switch
+                        {
+                            0b0001 => ForestRTile,
+                            0b0010 => ForestLTile,
+                            0b0011 => ForestLRTile,
+                            0b0100 => ForestBTile,
+                            0b0101 => ForestBRTile,
+                            0b0110 => ForestBLTile,
+                            0b1000 => ForestTTile,
+                            0b1001 => ForestTRTile,
+                            0b1010 => ForestTLTile,
+                            0b1100 => ForestTBTile,
+                            _ => null
+                        };
+                        ForestTilemap.SetTile(tilemapXy, forestTile);
+                    }
+                }
+            }
         }
     }
 }
