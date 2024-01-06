962bb59c35650ff4052247a8a5d646d20a885299
Beetroot Paul <paul@beetrootpaul.com>
2024-01-06 23:28:22 +0100

    day11: solution for part 2

--- unity/Assets/_/Day11/Day11Part2Code.cs
+++ unity/Assets/_/Day11/Day11Part2Code.cs
@@ -1,7 +1,7 @@
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Text;
 using TMPro;
 using UnityEngine;
 
@@ -15,16 +15,6 @@ namespace aoc2023.Day11
     //
     internal class Day11Part2Code : MonoBehaviour
     {
-        private enum InputData
-        {
-            Example2,
-            Example10,
-            Example100,
-            Example1000000,
-            Puzzle2,
-            Puzzle1000000,
-        }
-
         [Header("params")]
         [SerializeField]
         private InputData inputData;
@@ -37,8 +27,9 @@ namespace aoc2023.Day11
         {
             guiText.text = "...";
 
-            var data = Parse(inputData switch
+            var galaxies = Parse(inputData switch
             {
+                InputData.Test => "day11/test_in.txt",
                 InputData.Example2 => "day11/example2_2_in.txt",
                 InputData.Example10 => "day11/example2_10_in.txt",
                 InputData.Example100 => "day11/example2_100_in.txt",
@@ -48,19 +39,132 @@ namespace aoc2023.Day11
                 _ => "NOT_SET"
             });
 
-            // TODO: implement the actual solution
-            var result = data.Count;
+            Expand(galaxies, inputData switch
+            {
+                InputData.Test => 3,
+                InputData.Example2 => 2,
+                InputData.Example10 => 10,
+                InputData.Example100 => 100,
+                InputData.Example1000000 => 1000000,
+                InputData.Puzzle2 => 2,
+                InputData.Puzzle1000000 => 1000000,
+                _ => 1000000
+            });
 
-            print($"<color=green>result = {result}</color>");
-            guiText.text = $"{result}\nDONE";
+            print("== original X --> expanded X ==");
+            foreach (var g in galaxies)
+            {
+                print($"{g.OriginalX} --> {g.ExpandedX}");
+            }
+            print("== original Y / expanded XY==");
+            foreach (var g in galaxies)
+            {
+                print($"{g.OriginalY} --> {g.ExpandedY}");
+            }
+
+            var sum = SumShortestPathsBetween(galaxies);
+
+            print($"<color=green>sum = {sum}</color>");
+            guiText.text = $"{sum}\nDONE";
         }
 
-        private List<string> Parse(string inputFilePath)
+        private List<Galaxy> Parse(string inputFilePath)
         {
-            return File.ReadAllLines($"{Application.streamingAssetsPath}/{inputFilePath}")
+            var lines = File.ReadAllLines($"{Application.streamingAssetsPath}/{inputFilePath}")
                 .Select(line => line.Trim())
                 .Where(line => line.Length > 0)
                 .ToList();
+
+            var galaxies = new List<Galaxy>();
+            for (var row = 0; row < lines.Count(); row++)
+            {
+                for (var col = 0; col < lines[row].Length; col++)
+                {
+                    if (lines[row][col] == '#')
+                    {
+                        galaxies.Add(new Galaxy()
+                        {
+                            OriginalX = col,
+                            OriginalY = row,
+                        });
+                    }
+                }
+            }
+            return galaxies;
+        }
+
+        private void Expand(List<Galaxy> galaxies, long multiplier)
+        {
+            galaxies.Sort((g1, g2) =>
+                g1.OriginalX > g2.OriginalX
+                    ? 1
+                    : g1.OriginalX < g2.OriginalX
+                        ? -1
+                        : 0
+            );
+            var emptyCols = 0L;
+            var prevX = -1L;
+            foreach (var g in galaxies)
+            {
+                emptyCols += Math.Max(0, g.OriginalX - prevX - 1);
+                g.ExpandedX = g.OriginalX + emptyCols * (multiplier - 1);
+                prevX = g.OriginalX;
+            }
+
+            galaxies.Sort((g1, g2) =>
+                g1.OriginalY > g2.OriginalY
+                    ? 1
+                    : g1.OriginalY < g2.OriginalY
+                        ? -1
+                        : 0
+            );
+            var emptyRows = 0L;
+            var prevY = -1L;
+            foreach (var g in galaxies)
+            {
+                emptyRows += Math.Max(0, g.OriginalY - prevY - 1);
+                g.ExpandedY = g.OriginalY + emptyRows * (multiplier - 1);
+                prevY = g.OriginalY;
+            }
+        }
+
+        private long SumShortestPathsBetween(List<Galaxy> galaxies)
+        {
+            var sum = 0L;
+
+            for (var i = 0; i < galaxies.Count; i++)
+            {
+                for (var j = i + 1; j < galaxies.Count; j++)
+                {
+                    sum += Galaxy.ShortestPathBetween(galaxies[i], galaxies[j]);
+                }
+            }
+
+            return sum;
+        }
+
+        private enum InputData
+        {
+            Test,
+            Example2,
+            Example10,
+            Example100,
+            Example1000000,
+            Puzzle2,
+            Puzzle1000000,
+        }
+
+        private class Galaxy
+        {
+            internal long OriginalX;
+            internal long OriginalY;
+            internal long ExpandedX;
+            internal long ExpandedY;
+
+            public static long ShortestPathBetween(Galaxy g1, Galaxy g2)
+            {
+                return Math.Abs(g1.ExpandedX - g2.ExpandedX) + Math.Abs(g1.ExpandedY - g2.ExpandedY);
+            }
         }
     }
 }
