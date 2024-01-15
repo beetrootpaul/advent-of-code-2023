6d60313b80e31ecb4495432281b534e4da69ccbd
Beetroot Paul <paul@beetrootpaul.com>
2024-01-15 01:19:30 +0100

    day21: solve the puzzle for part 1

--- godot/day21/Day21Part1.cs
+++ godot/day21/Day21Part1.cs
@@ -33,20 +33,29 @@ internal partial class Day21Part1 : Node
     private Vector2I _start;
     private bool[][] _rocks;
     private bool[][] _reached;
+    private bool[][][] _processed;
 
     public override async void _Ready()
     {
+        var inputFile = _myInputFile switch
+        {
+            InputFile.Example => "day21/example1_in.txt",
+            InputFile.Puzzle => "day21/puzzle1_in.txt",
+            _ => "NOT_SET"
+        };
+        var steps = _myInputFile switch
+        {
+            InputFile.Example => 6,
+            InputFile.Puzzle => 64,
+            _ => 1
+        };
+
         Input.ParseInputEvent(new InputEventAction
             { Action = "cycle_debug_menu", Pressed = true });
 
         _hud?.SetText("...");
 
-        Parse(_myInputFile switch
-        {
-            InputFile.Example => "day21/example1_in.txt",
-            InputFile.Puzzle => "day21/puzzle1_in.txt",
-            _ => "NOT_SET"
-        });
+        Parse(inputFile, steps);
         GD.Print(_start);
 
         _visualization?.Visualize(_rocks, _start, _reached);
@@ -56,14 +65,8 @@ internal partial class Day21Part1 : Node
             _mainCamera.AdjustZoomToContent(_visualization.GetRect());
         }
 
-        foreach (var _ in FindPositionsAfterSteps(_myInputFile switch
-                 {
-                     InputFile.Example => 6,
-                     InputFile.Puzzle => 64,
-                     _ => 1
-                 }))
+        foreach (var _ in FindPositionsAfterSteps(steps))
         {
-            GD.Print("step");
             await Task.Delay(50);
             _visualization?.Visualize(_rocks, _start, _reached);
         }
@@ -73,10 +76,10 @@ internal partial class Day21Part1 : Node
         var reachedAmount =
             _reached.Select(row => row.Count(isTileReached => isTileReached)).Sum();
         GD.Print("reachedAmount: ", reachedAmount);
-        _hud.SetText($"RESULT\n{reachedAmount}");
+        _hud?.SetText($"RESULT\n{reachedAmount}");
     }
 
-    private void Parse(string inputFile)
+    private void Parse(string inputFile, int steps)
     {
         var rawInputData = FileAccess.Open(inputFile, FileAccess.ModeFlags.Read)
             .GetAsText();
@@ -91,14 +94,30 @@ internal partial class Day21Part1 : Node
 
         _rocks = new bool[_size.Y][];
         _reached = new bool[_size.Y][];
+        _processed = new bool[steps + 1][][];
+        for (var s = 0; s <= steps; s++)
+        {
+            _processed[s] = new bool[_size.Y][];
+        }
+
         for (var y = 0; y < _size.Y; y++)
         {
             _rocks[y] = new bool[_size.X];
             _reached[y] = new bool[_size.X];
+            for (var s = 0; s <= steps; s++)
+            {
+                _processed[s][y] = new bool[_size.X];
+            }
+
             for (var x = 0; x < _size.X; x++)
             {
                 _rocks[y][x] = inputLines[y][x] == '#';
                 _reached[y][x] = false;
+                for (var s = 0; s <= steps; s++)
+                {
+                    _processed[s][y][x] = false;
+                }
+
                 if (inputLines[y][x] == 'S')
                 {
                     _start = new Vector2I(x, y);
@@ -110,33 +129,39 @@ internal partial class Day21Part1 : Node
     private IEnumerable FindPositionsAfterSteps(int steps)
     {
         var yieldCountdown = 0;
-        
+
         var directions = new List<Vector2I>
             { Vector2I.Right, Vector2I.Down, Vector2I.Left, Vector2I.Up };
 
         Stack<(Vector2I position, int stepsLeft)> s = new();
         s.Push((_start, steps));
+
         while (s.Count > 0)
         {
             var (position, stepsLeft) = s.Pop();
 
+
             if (stepsLeft <= 0)
             {
                 _reached[position.Y][position.X] = true;
-                continue;
             }
-
-            foreach (var direction in directions)
+            else
             {
-                var next = position + direction;
-                if (next >= Vector2I.Zero && next < _size && !_rocks[next.Y][next.X])
+                foreach (var direction in directions)
                 {
-                    s.Push((next, stepsLeft - 1));
+                    var next = position + direction;
+                    if (next >= Vector2I.Zero && next < _size &&
+                        !_rocks[next.Y][next.X] &&
+                        !_processed[stepsLeft - 1][next.Y][next.X])
+                    {
+                        s.Push((next, stepsLeft - 1));
+                    }
                 }
-                
             }
 
-            if (yieldCountdown++ < 50) continue;
+            _processed[stepsLeft][position.Y][position.X] = true;
+
+            if (yieldCountdown++ < 500) continue;
             yield return null;
             yieldCountdown = 0;
         }
